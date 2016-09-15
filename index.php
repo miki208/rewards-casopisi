10016eceae3bfbba3be7a6fa93b7dcdcf8160822
miki208 <miloss208@gmail.com>
2016-09-15 13:59:07 +0200

    Add simple SDK setup

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
   </head>
 
   <body>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '201331556950198',
+          xfbml      : true,
+          version    : 'v2.7'
+        });
+      };
+
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/sr_RS/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+
     <h1>Welcome</h1>
   </body>
 </html>
